38fae6de6fff4f62964e76584c4d33263575c0b1
Alan McGovern <alan@xamarin.com>
2013-08-19 11:52:09 -0400

    [MacPlatform] Remove some duplicated NSApplication.Init calls
    We need to enforce that NSApplication is only initalised at application
    startup. We still call NSApplication.Init more than once and we still
    do it lazily which is Bad. We can end up (re)initializing on the wrong
    thread and break monomac.

--- main/src/addins/MacPlatform/MacPlatform.cs
+++ main/src/addins/MacPlatform/MacPlatform.cs
@@ -592,8 +592,6 @@ end tell", directory.ToString ().Replace ("\"", "\\\"")));
 
 		internal override MainToolbar CreateMainToolbar (Gtk.Window window)
 		{
-			NSApplication.Init ();
-			
 			NSWindow w = GtkQuartz.GetWindow (window);
 			w.IsOpaque = false;
 			
