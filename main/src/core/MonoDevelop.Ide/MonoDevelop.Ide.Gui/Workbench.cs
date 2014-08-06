53499d127596cd0054ae52b6ed8c6ab9ba03bca7
Lukasz Paczkowski <lukasz@unity3d.com>
2014-08-06 11:00:13 +0200

    Fixed issue where the same file could be open twice.
    If you have a file A open and then open a solution where file A is in
    the user preferences, then file A will be opened twice.
    
    This fix makes sure that a file is not already open when loading the
    user preferences for a solution.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Workbench.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Workbench.cs
@@ -928,7 +928,7 @@ namespace MonoDevelop.Ide.Gui
 				
 				foreach (DocumentUserPrefs doc in prefs.Files.Distinct (new DocumentUserPrefsFilenameComparer ())) {
 					string fileName = baseDir.Combine (doc.FileName).FullPath;
-					if (File.Exists (fileName)) {
+					if (GetDocument(fileName) == null && File.Exists (fileName)) {
 						var view = IdeApp.Workbench.BatchOpenDocument (pm, fileName, doc.Line, doc.Column);
 						if (fileName == currentFileName)
 							currentView = view;
